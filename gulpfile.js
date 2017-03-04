dd3b34a5b8ca026d500e31020b6c56a8b15d39cf
shedm <shedm@hismith.ru>
2017-03-04 20:15:10 +0300

    Drop other things

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,7 @@ var gulp = require('gulp'),
   watch = require('gulp-watch'),
   prettify = require('gulp-html-prettify');
 
-var ccm = require('./ccm/ccm');
-var ccmGulp = require('./ccm/ccm-gulp');
-
 gulp.task('build', function () {
-  gulp.src('./pages/*.json')
-    .pipe(ccmGulp());
-
   gulp.src('./pages/*.less')
     .pipe(less())
     .pipe(gulp.dest('./pages'));
@@ -27,23 +21,6 @@ gulp.task('build', function () {
 
 
 gulp.task('dev', ['build'], function () {
-
-  gulp.watch('./pages/*.json').on('change', function (event) {
-    // event.type : added, changed, or deleted
-    console.log(event.type + ' ' + event.path);
-
-    if (event.type !== 'deleted') {
-      // compile
-      ccm(undefined, event.path);
-    }
-
-    gulp.src('./pages/*.html')
-      .pipe(prettify({
-        indent_char: ' ',
-        indent_size: 2
-      }))
-      .pipe(gulp.dest('./pages/'));
-  });
 
   gulp.watch('./pages/*.less').on('change', function (event) {
     // event.type : added, changed, or deleted
